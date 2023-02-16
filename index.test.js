01d7c0f3d6d09457b58e8f67a072d13d7f8a9bd3
soollyy <solianatesema1@gmail.com>
2023-02-16 16:07:57 +0000

    wrote code and got code to pass all tests

--- index.test.js
+++ index.test.js
@@ -5,20 +5,32 @@ describe('Band and Musician Models', () => {
     /**
      * Runs the code prior to all tests
      */
+
     beforeAll(async () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
         await sequelize.sync({ force: true });
+        
     })
 
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const newBand = await Band.create({
+            name: "campRock",
+            genre: "classical"
+        });
+        expect(newBand.name).toBe('campRock');
+        expect(newBand.genre).toBe('classical');
     })
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const newMusician = await Musician.create({
+            name: "soliana tesema",
+            instrument: "piano"
+        });
+        expect(newMusician.name).toBe('soliana tesema');
+        expect(newMusician.instrument).toBe('piano');
     })
 })
